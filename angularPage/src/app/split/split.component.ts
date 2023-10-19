6229fdf57574faca934c84b9b96fe91eccad506c
benjaespina98 <83318957+benjaespina98@users.noreply.github.com>
2023-10-19 01:07:03 -0300

    Se elimina categoría

--- angularPage/src/app/split/split.component.ts
+++ angularPage/src/app/split/split.component.ts
@@ -6,15 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./split.component.scss']
 })
 export class SplitComponent {
-  expenses: { name: string; expense: number, category: string }[] = [];
+  expenses: { name: string; expense: number }[] = [];
   totalExpense = 0;
   results: { debtor: string; creditor: string; amount: number }[] = [];
   name: string = '';
   expense: number | null = null;
-  category: string = ''; // Variable para la categoría
-  categories: string[] = []; // Lista de categorías
-  selectedCategory: string = ''; // Variable para la última categoría seleccionada
-  showCategoryInput = false;
 
   addExpense() {
     if (!this.name) {
@@ -26,41 +22,21 @@ export class SplitComponent {
       return;
     }
 
-    if (!this.selectedCategory) {
-      alert('Por favor, selecciona una categoría...');
-      return;
-    }
-
-    // Verificar si la categoría ya existe en la lista de categorías
-    if (!this.categories.includes(this.selectedCategory)) {
-      // Si la categoría no existe, toma el valor de la categoría personalizada
-      this.category = this.selectedCategory;
-    } else {
-      // Si la categoría existe, usa el valor seleccionado del combo
-      this.category = this.selectedCategory;
-    }
-
     const expenseValue = Number(this.expense);
-
-    // Verificar si la categoría ya existe en la lista de categorías
-    if (!this.categories.includes(this.category)) {
-      this.categories.push(this.category);
-    }
-
-    const existingExpense = this.expenses.find((item) => item.name === this.name && item.category === this.category);
+    
+    const existingExpense = this.expenses.find((item) => item.name === this.name);
 
     if (existingExpense) {
       // Si la persona ya ha ingresado un gasto, sumar el nuevo gasto al gasto existente
       existingExpense.expense += expenseValue;
     } else {
       // Si no, agregar un nuevo gasto
-      this.expenses.push({ name: this.name, expense: expenseValue, category: this.category });
+      this.expenses.push({ name: this.name, expense: expenseValue });
     }
 
     this.updateExpenseList();
     this.name = '';
     this.expense = null;
-    this.selectedCategory = ''; // Restablece la categoría seleccionada
   }
 
   updateExpenseList() {
@@ -110,7 +86,7 @@ export class SplitComponent {
     }
   }
 
-  deleteExpense(expenseItem: { name: string, expense: number, category: string }) {
+  deleteExpense(expenseItem: { name: string; expense: number }) {
     const index = this.expenses.indexOf(expenseItem);
     if (index !== -1) {
       this.expenses.splice(index, 1);
@@ -119,16 +95,5 @@ export class SplitComponent {
   clearExpenses() {
     this.expenses = [];
     this.results = [];
-    this.category = '';
-    this.categories = [];
   }
-
-
-  toggleCategoryInput() {
-    this.showCategoryInput = !this.showCategoryInput;
-    if (!this.showCategoryInput) {
-      this.category = '';
-    }
-  }
-
 }
