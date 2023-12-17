0ed6bed217850c9bdb2288dfce8dbeb5f96b4d86
benjaespina98 <83318957+benjaespina98@users.noreply.github.com>
2023-12-17 20:38:58 -0300

    split alone

--- angularPage/src/app/app-routing.module.ts
+++ angularPage/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingComponent } from './landing/landing.component';
 import { SplitComponent } from './split/split.component';
 
 
 const routes: Routes = [
-  { path: '', component: LandingComponent},
   {path: 'split', component: SplitComponent}
 ];
 
